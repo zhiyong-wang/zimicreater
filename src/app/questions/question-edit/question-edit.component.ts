17f6163c9a8d26583cba54c384a0c922d45b0c10
zhiyong-wang <zywang0209@163.com>
2018-09-25 15:51:27 +0800

    add question add function

--- src/app/questions/question-edit/question-edit.component.ts
+++ src/app/questions/question-edit/question-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { WordItem } from '../WordItem';
-
+import { QuestionService } from '../question.service'
 @Component({
   selector: 'app-question-edit',
   templateUrl: './question-edit.component.html',
@@ -12,12 +12,19 @@ export class QuestionEditComponent implements OnInit {
 
 
   errer_message:string='';  
-  wordItem:WordItem={Question_ID:-1,Midi:'000',Question:'',Answer:''};
+  wordItem:WordItem={Midi:'000',Question:'',Answer:''};
   thismessage:string="null"
-  
+ 
+
+  onKey():void{
+       this.wordItem.Midi=this.wordItem.Midi.replace(/\s+/g,'')                                    
+       this.wordItem.Question=this.wordItem.Question.replace(/\s+/g,'')
+       this.wordItem.Answer= this.wordItem.Answer.replace(/\s+/g,'')
+     }
   add(): void {      
   	   if(this.wordItem.Midi&&this.wordItem.Question&&this.wordItem.Answer){
-          // this.service.addWordItem(this.wordItem);
+          this.service.addwordItem(this.wordItem)
+             .subscribe(()=>{this.cleanwordItem()});
           this.thismessage="It is ok"
         }
         else{
@@ -25,17 +32,18 @@ export class QuestionEditComponent implements OnInit {
         } 
 
    };
-   onKey():void{
-       this.wordItem.Midi=this.wordItem.Midi.replace(/\s+/g,'')                                    
-       this.wordItem.Question=this.wordItem.Question.replace(/\s+/g,'')
-       this.wordItem.Answer= this.wordItem.Answer.replace(/\s+/g,'')
-     }
+   cleanwordItem(): void {
+     this.wordItem={Midi:'',Question:'',Answer:''}; 
+   }
+
+
+
 
   constructor(
 
     private route:ActivatedRoute,
     private router:Router,
-   // private service: QuestionService
+    private service: QuestionService
    )  
     {}
 
---
+++ src/app/questions/question.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { WordItem} from './WordItem'
+
+const httpOptions = {
+	  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+	};
+
+const httpOptions1 = {
+	  headers: new HttpHeaders({ 'Content-Type': 'string' })
+	};
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class QuestionService {
+  private httpsUrl = 'http://localhost:5757/weapp/tianzi';
+  
+ 	addwordItem (wordItem:WordItem): Observable<{}> {
+        let Url="http://localhost:5757/weapp/question_add"
+  		return this.http.post<{}>(Url, wordItem, httpOptions).pipe(
+        catchError(this.handleError<WordItem>('addWordItem'))
+ 	 );
+
+	}
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+ 
+ 
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+ 
+     // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+
+  constructor( private http: HttpClient) {}
+
+}
